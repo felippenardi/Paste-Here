821f2fc2e524d7a99ae9d86a2207fa1d719dfa19
Felippe Nardi <felippe@nardi.me>
2013-01-03 14:57:25 +0000

    Fixing Google Analytics

--- pastehere.js
+++ pastehere.js
@@ -37,6 +37,19 @@ if (Meteor.is_client) {
                     }, 10);
             }
         });
+
+        // Google Analytics
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-37388600-1']);
+        _gaq.push(['_setDomainName', 'meteor.com']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+        //end Google Analytics
     });
 
     Template.drops.drops = function () {
