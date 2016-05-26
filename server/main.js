7aff7ca608a7c3174b48e0052b085f2e8fe5eb05
aj07mm <aj07mm@hotmail.com>
2016-05-26 14:15:42 -0300

    fix

--- server/main.js
+++ server/main.js
@@ -2,9 +2,6 @@ Drops = new Meteor.Collection("drops");
 
 Drops.allow({
     insert: function (userId, doc) {
-
-        var currentDrop = Drops.findOne({drop:doc.drop});
-
         if (doc.drop == '') {
             return false;
         }
